a3d0921bbeafc7f258b9d5671356d3afa51f8ed2
xelambert <xelambert@gmail.com>
2023-05-29 01:07:33 +0300

    other files

--- lab02/v0/start.sh
+++ lab02/v0/start.sh
@@ -4,5 +4,5 @@ stat=$(qsub-v arg1="100" -W depend=on:100 stat.sh)
 
 for i in {1..100}
 do
-	qsub -v, arg1="$i" -W depend=beforeany:$stat task.sh
+	qsub -v arg1="$i" -W depend=beforeany:$stat task.sh
 done
