71745d0eaf849cfaa1512dfc4aeff972127984de
xelambert <xelambert@gmail.com>
2023-05-29 00:23:36 +0300

    params

--- lab02/algo.sh
+++ lab02/algo.sh
@@ -4,5 +4,5 @@ stat=$(qsub -v arg1="100" -W depend=on:100 statistics.sh)
 
 for i in {1..100}
 do
-	qsub -v, arg1="$i" -W depend=beforeany:$stat runTask.sh
+	qsub -v arg1="$i" -W depend=beforeany:$stat runTask.sh
 done
